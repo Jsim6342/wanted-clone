a332c78da8e569562ad296f6ef77c2c7fe1874af
Jsim6342 <snowdrop6342@gmail.com>
2021-11-04 00:08:46 +0900

    [#1] mysql 설정 부분 yml 분리 및 .gitignore 등록

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(DemoApplication.class, args);
+        // property 파일 설정.
+        String PROPERTIES = "spring.config.location=classpath:/application.yml"
+                +",classpath:/mysql.yml";
+
+        new SpringApplicationBuilder(DemoApplication.class)
+                .properties(PROPERTIES)
+                .run(args);
+
 
         // 메모리 사용량 출력
         long heapSize = Runtime.getRuntime().totalMemory();
