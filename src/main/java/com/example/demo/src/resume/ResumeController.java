e9c13dee8e999cb110bbd337e18128357859e760
Jsim6342 <snowdrop6342@gmail.com>
2021-11-06 17:53:42 +0900

    [#57] 이력서 도메인 패키지 틀 생성

---
+++ src/main/java/com/example/demo/src/resume/ResumeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.src.resume;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/resumes")
+@RequiredArgsConstructor
+public class ResumeController {
+
+
+}
---
+++ src/main/java/com/example/demo/src/resume/ResumeProvider.java
@@ -0,0 +1,7 @@
+package com.example.demo.src.resume;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResumeProvider {
+}
